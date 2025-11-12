96f43e391a0f7769741332946b8eb4da10181245
Tejas Gupta <tgupta39@wisc.edu>
2025-11-11 21:05:29 -0600

    Completed Welcome section, not responsive yet.

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -7,7 +7,7 @@ export default function Home() {
     <div className={styles.home}>
       <div className={styles.titleSection}>
         <div className={styles.title}>
-          <h1>PLAYGROUND MARKETING FIRM</h1>
+          <h1><strong>PLAYGROUND MARKETING FIRM</strong></h1>
           <Image 
             src="/images/Cross.png" 
             alt="My Photo" 
@@ -16,22 +16,47 @@ export default function Home() {
           />
         </div>
       </div>
-
-      {/* Video Section */}
-      <div className={styles.videoSection}>
-        <video
-          className={styles.backgroundVideo}
-          src="/videos/JayWalking.MP4"
-          autoPlay
-          muted
-          loop
-          playsInline
-        />
-      </div>
-
-      {/* Content below video */}
       <div className={styles.contentSection}>
+        {/* Video Section */}
+        <div className={styles.videoSection}>
+          <video
+            className={styles.backgroundVideo}
+            src="/videos/JayWalking.MP4"
+            autoPlay
+            muted
+            loop
+            playsInline
+          />
+        </div>
 
+        {/* Welcome Section */}
+        <div className={styles.welcomeSection}>
+          <h1 className={styles.welcomeTop}><i>Welcome To...</i></h1>
+          <h1 className={styles.welcomeBottom}>The Playground</h1>
+          <div className={styles.welcomeDescription}>
+            <p>
+              It’s an opportunity to tell the story behind the business or describe a special 
+              service or product it offers. You can use this section to share the company's history or 
+              highlight a particular feature that sets it apart from competitors.
+            </p>
+            <button className={styles.scheduleButton}>
+              SCHEDULE A CALL
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="1"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                width="1rem"
+                height="1rem"
+              >
+                <path d="M5 12h14M13 5l7 7-7 7" />
+              </svg>
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
